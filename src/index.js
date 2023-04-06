95587118a5c243c76289b3f8513306c119f9787d
AndriiKostenko84 <a.kostenko84@gmail.com>
2023-04-06 15:34:23 +0300

    import

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ import { PixabayApi } from './fetchPhoto';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import throttle from 'lodash.throttle';
+
+
+const formEl = document.querySelector('.search-form');
+const gallaryDiv = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const bodyEl = document.querySelector('body');
+const checkEl = document.querySelector('.autoload_check');
+
+
+const pixabayApi = new PixabayApi();
+let inputValue = '';
+let gallery = new simpleLightbox('.gallery a', {
+    captionsData: "title"
+});
